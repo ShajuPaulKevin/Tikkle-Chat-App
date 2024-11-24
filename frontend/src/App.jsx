1926b4bd23de8766af63de2cc08c58ef06417844
kevinpaul <kevinpauljet@gmail.com>
2024-11-23 21:34:38 -0500

    added routes for the pages in clientside using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "../src/pages/login/Login";
+import SignUp from "../src/pages/signup/Signup";
 import "./App.css";
 import Home from "./pages/home/home";
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+      </Routes>
     </div>
   );
 }
