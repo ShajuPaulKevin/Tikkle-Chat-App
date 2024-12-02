7c66ed1007a1535c1311ab3dd19d9a3351ed8546
kevinpaul <kevinpauljet@gmail.com>
2024-12-02 11:46:56 -0500

    displays messages properly and like and actual conversation. search feature added

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,18 +1,34 @@
 import React from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
+import { extractTime } from "../../utils/extractTime";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  console.log(authUser, selectedConversation);
+  const formattedTime = extractTime(message.createdAt);
+  const fromMe = message.senderId === authUser._id;
+  console.log(fromMe);
+
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePicture
+    : selectedConversation?.profilePicture;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
   return (
-    <div className="chat chat-end ">
+    <div className={`chat ${chatClassName} `}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            src="https://avatar.iran.liara.run/public/boy?username=John"
-            alt=""
-          />
+          <img src={profilePic} alt="" />
         </div>
       </div>
-      <div className="chat-bubble text-white bg-blue-500">Hey whats up?</div>
-      <div className="chat-footer opacity-50 text-xs gap-1 flex items-center">12:42</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
+      </div>
+      <div className="chat-footer opacity-50 text-xs gap-1 flex items-center">
+        {formattedTime}
+      </div>
     </div>
   );
 };
